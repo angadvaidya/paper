e9641359b7e817e66bbcffbe7c46ccc03a2e1ac1
unknown <angadvaiday@gmail.com>
2023-07-05 15:07:42 +0530

    one commit

---
+++ Av.java
@@ -0,0 +1,14 @@
+public class Av{
+int Id;
+String name;
+float salary;
+int A(int Id,String name,float salary){
+System.out.println("Id no: "+Id +"Name:"+name+"salary:"+salary);
+return0;
+}
+public static void main(String[] args){
+
+Av P=new Av();
+P.A(5,"java",80000);
+}
+}
